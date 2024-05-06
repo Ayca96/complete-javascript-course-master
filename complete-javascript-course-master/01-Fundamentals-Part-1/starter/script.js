6c19f648862673351aadbfcc9140e675e651aec1
Ayca96 <aycakubra1@gmail.com>
2024-05-06 16:33:21 +0200

    first commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,145 @@
+/*let js = 'amazing';
+ console.log( 40 + 8 + 23 - 10);
+
+ console.log("Jonas");
+ console.log(23);
+ 
+let firstName = " Matilda"
+// let first = 'Jonas'
+// let firstNamePerson
+// let first_name_person
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// let 3years = 3;  Degisken adlari sayiyla baslamaz, yalnizca rakamlar, harfler, alt cizgiler ve dolar icerebilir. 
+
+// Variable name conventions
+
+let jonas_matilda = 'JM';
+let $function = 27;
+let Person = 'Jonas';  //Simdilik büyük harfle baslama, bunun baska anlami var.
+let person = 'Jonas';
+
+
+let PI = 3.1415; 
+// Tamami büyük harfle yazilan degiskenler asla degismeyecegini bildigimiz sabitler icin ayrilmistir. 
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';  //Bu yazim sekli alttakine göre daha iyi.
+
+let job1 = 'programmer';
+let job2 = 'teacher';  
+
+console.log(myFirstJob);
+
+
+// let country = 'Turkey'
+// let continent = 'Europa'
+// let population = '82 Milion'
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// console.log('country' + 'continent'); Code Challenge kismindaki görevi yaptim.
+
+// true;
+// console.log(true);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+// job = 'teacher';  sakin bu sekilde yazma, mutlaka let, const var variable belirt! yanlis bir yazim sekli. 
+
+// //! Math Operators
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2**3);
+// // 2 ** 3 means 2 to the power of 3 = 2*2*2* 
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
+
+
+// //! Assignment Operators
+
+// let x = 10 + 5; //15
+// x += 10; // x =  x + 10 = 25
+// x *= 4; // x = x*4 = 100
+// x++; // x = x + 1 = 101
+// x--; // x = x - 1 = 99
+// console.log(x);
+
+// //! Comparison Operators
+
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+/*const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; //x = y = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah,  averageAge);*/
+
+/*const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;*/
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
