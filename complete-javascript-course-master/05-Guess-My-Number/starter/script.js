bfc798148ff44b8d07acb82fc0afdb1b240f05b8
Ayca96 <aycakubra1@gmail.com>
2024-06-09 16:09:02 +0200

    click events

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,11 +1,25 @@
 'use strict';
 
-console.log(document.querySelector(".message").textContent);  
-document.querySelector(".message").textContent= "🎉Correct Number!"
-console.log(document.querySelector(".message").textContent); 
+// console.log(document.querySelector(".message").textContent);  
+// document.querySelector(".message").textContent= "🎉Correct Number!"
+// console.log(document.querySelector(".message").textContent); 
 
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
+// document.querySelector(".number").textContent = 13;
+// document.querySelector(".score").textContent = 10;
 
-document.querySelector(".guess").value = 23
-console.log(document.querySelector(".guess").value); 
+// document.querySelector(".guess").value = 23
+// console.log(document.querySelector(".guess").value); 
+
+//document.querySelector(".check").addEventListener("click", function (){
+
+//console.log( document.querySelector(".guess").value);
+//document.querySelector(".message").textContent= "🎉Correct Number!" 
+//Burda ekranda degisir ama consol yapmadigimiz icin consolda görmeyiz tek fark bu.
+//});
+
+document.querySelector(".check").addEventListener("click", function (){
+
+  const guess = Number(document.querySelector(".guess").value)
+  console.log(guess, typeof guess); // stringti basina Number ekleyerek number a cevirdik.
+ 
+  });
