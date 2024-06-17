aefc60d1c0597b4587e2f0e0447e2f6482ab8789
Ayca96 <aycakubra1@gmail.com>
2024-06-17 21:47:02 +0200

    commit

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -27,4 +27,24 @@ const displayMessage = function (message) {
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+  console.log(guess, typeof guess);}
+
+
+    // When there is no input
+    if (!guess) {
+      // document.querySelector('.message').textContent = '⛔️ No number!';
+      displayMessage('⛔️ No number!');
+  
+      // When player wins
+    } else if (guess === secretNumber) {
+      // document.querySelector('.message').textContent = '🎉 Correct Number!';
+      displayMessage('🎉 Correct Number!');
+      document.querySelector('.number').textContent = secretNumber;
+  
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
+  
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
